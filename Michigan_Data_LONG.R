2fbc49a161923da5ca76aa7ba1991984ff9e7a28
dbetebenner <dbetebenner@gmail.com>
2014-09-21 14:55:49 -0400

    Adding Michigan_Data_LONG

---
+++ Michigan_Data_LONG.R
@@ -0,0 +1,39 @@
+######################################################################################
+###
+### R script for creations of Michigan LONG data file
+###
+######################################################################################
+
+### Load Packages
+
+require(data.table)
+
+
+### Load raw data files
+
+MEAP_2012_2013_SGP <- fread("Data/Base_Files/MEAP_2012_13_SGP.txt", sep="|", colClasses=rep("character", 6))
+MEAP_2013_2014_SGP <- fread("Data/Base_Files/MEAP_2013_14_SGP.txt", sep="|", colClasses=rep("character", 6))
+MME_2012_2013_SGP <- fread("Data/Base_Files/MME_2012_13_SGP.txt", sep="|", colClasses=rep("character", 6))
+MME_2013_2014_SGP <- fread("Data/Base_Files/MME_2013_14_SGP.txt", sep="|", colClasses=rep("character", 6))
+
+
+### Tidy up data
+
+MME_2012_2013_SGP[,GRADE:="EOCT"]
+MME_2012_2013_SGP[,SCALE_SCORE:=as.numeric(SCALE_SCORE)]
+MME_2013_2014_SGP[,GRADE:="EOCT"]
+MME_2013_2014_SGP[,SCALE_SCORE:=as.numeric(SCALE_SCORE)]
+MEAP_2012_2013_SGP[,GRADE:=as.character(as.numeric(GRADE))]
+MEAP_2012_2013_SGP[,SCALE_SCORE:=as.numeric(SCALE_SCORE)]
+MEAP_2013_2014_SGP[,SCALE_SCORE:=as.numeric(SCALE_SCORE)]
+
+
+### Stack data up:
+
+Michigan_Data_LONG <- rbindlist(list(MEAP_2012_2013_SGP, MEAP_2013_2014_SGP, MME_2012_2013_SGP, MME_2013_2014_SGP))
+setnames(Michigan_Data_LONG, 1, "ID")
+setkey(Michigan_Data_LONG, VALID_CASE, CONTENT_AREA, YEAR, ID)
+
+### Save results
+
+save(Michigan_Data_LONG, file="Data/Michigan_Data_LONG.Rdata")
