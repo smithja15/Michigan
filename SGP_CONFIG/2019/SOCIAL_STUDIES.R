1e38b013736388c77735aacec6ef0ab4939b04a9
Damian Betebenner <dbetebenner@nciea.org>
2019-08-10 22:57:05 -0400

    Fixing up configs

--- SGP_CONFIG/2019/SOCIAL_STUDIES.R
+++ SGP_CONFIG/2019/SOCIAL_STUDIES.R
@@ -8,10 +8,10 @@ SOCIAL_STUDIES.2018_2019.config <- list(
 	SOCIAL_STUDIES.2018_2019=list(
 		sgp.content.areas=c('SOCIAL_STUDIES', 'SOCIAL_STUDIES', 'SOCIAL_STUDIES'),
 		sgp.panel.years=c('2013_2014', '2015_2016', '2018_2019'),
-		sgp.grade.sequences=list(c('6', '8', '11'))),
-		sgp.projection.grade.sequences=as.list("NO_PROJECTIONS"),
+		sgp.grade.sequences=list(c('6', '8', '11')),
+		sgp.projection.grade.sequences="NO_PROJECTIONS"),
 	SOCIAL_STUDIES.2018_2019=list( 
 		sgp.content.areas=c('SOCIAL_STUDIES', 'SOCIAL_STUDIES', 'SOCIAL_STUDIES'),
        	sgp.panel.years=c('2015_2016', '2018_2019'),
-       	sgp.grade.sequences=list(c('5','8'))),
-		sgp.projection.grade.sequences=as.list("NO_PROJECTIONS"))
+       	sgp.grade.sequences=list(c('5','8')),
+		sgp.projection.grade.sequences="NO_PROJECTIONS")
