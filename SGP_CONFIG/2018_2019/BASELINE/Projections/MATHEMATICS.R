c376a83cf002615d16901b550b19cd1ff43b21a3
Damian Betebenner <dbetebenner@nciea.org>
2021-07-30 16:32:34 -0400

    Getting years into the right format

--- SGP_CONFIG/2018_2019/BASELINE/Projections/MATHEMATICS.R
+++ SGP_CONFIG/2018_2019/BASELINE/Projections/MATHEMATICS.R
@@ -1,85 +1,85 @@
-########################################################################################
-###                                                                                  ###
-###     Configurations for STRAIGHT (skip-year) MATHEMATICS projections in 2019      ###
-###                                                                                  ###
-########################################################################################
+#############################################################################################
+###                                                                                       ###
+###     Configurations for STRAIGHT (skip-year) MATHEMATICS projections in 2018_2019      ###
+###                                                                                       ###
+#############################################################################################
 
-MATHEMATICS_2019.config <- list(
-    MATHEMATICS.2019 = list(
+MATHEMATICS_2018_2019.config <- list(
+    MATHEMATICS.2018_2019 = list(
         sgp.content.areas=rep("MATHEMATICS", 2),
         sgp.baseline.content.areas=rep("MATHEMATICS", 2),
-        sgp.panel.years=c("2017", "2019"),
-        sgp.baseline.panel.years=c("2017", "2019"),
+        sgp.panel.years=c("2016_2017", "2018_2019"),
+        sgp.baseline.panel.years=c("2016_2017", "2018_2019"),
         sgp.grade.sequences=list(c("3", "5")),
         sgp.baseline.grade.sequences=list(c("3", "5")),
         sgp.projection.baseline.content.areas="MATHEMATICS",
-        sgp.projection.baseline.panel.years="2019",
+        sgp.projection.baseline.panel.years="2018_2019",
         sgp.projection.baseline.grade.sequences=list(c("3")),
         sgp.projection.sequence="MATHEMATICS_GRADE_3"),
-    MATHEMATICS.2019 = list(
+    MATHEMATICS.2018_2019 = list(
         sgp.content.areas=rep("MATHEMATICS", 3),
         sgp.baseline.content.areas=rep("MATHEMATICS", 3),
-        sgp.baseline.panel.years=c("2016", "2017", "2019"),
-        sgp.panel.years=c("2016", "2017", "2019"),
+        sgp.baseline.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
+        sgp.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
         sgp.grade.sequences=list(c("3", "4", "6")),
         sgp.baseline.grade.sequences=list(c("3", "4", "6")),
         sgp.projection.baseline.content.areas=rep("MATHEMATICS", 2),
-        sgp.projection.baseline.panel.years=c("2018", "2019"),
+        sgp.projection.baseline.panel.years=c("2017_2018", "2018_2019"),
         sgp.projection.baseline.grade.sequences=list(c("3", "4")),
         sgp.projection.sequence="MATHEMATICS_GRADE_4"),
-    MATHEMATICS.2019 = list(
+    MATHEMATICS.2018_2019 = list(
         sgp.content.areas=rep("MATHEMATICS", 3),
         sgp.baseline.content.areas=rep("MATHEMATICS", 3),
-        sgp.baseline.panel.years=c("2016", "2017", "2019"),
-        sgp.panel.years=c("2016", "2017", "2019"),
+        sgp.baseline.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
+        sgp.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
         sgp.grade.sequences=list(c("4", "5", "7")),
         sgp.baseline.grade.sequences=list(c("4", "5", "7")),
         sgp.projection.baseline.content.areas=rep("MATHEMATICS", 2),
-        sgp.projection.baseline.panel.years=c("2018", "2019"),
+        sgp.projection.baseline.panel.years=c("2017_2018", "2018_2019"),
         sgp.projection.baseline.grade.sequences=list(c("4", "5")),
         sgp.projection.sequence="MATHEMATICS_GRADE_5"),
-    MATHEMATICS.2019 = list(
+    MATHEMATICS.2018_2019 = list(
         sgp.content.areas=rep("MATHEMATICS", 3),
         sgp.baseline.content.areas=rep("MATHEMATICS", 3),
-        sgp.baseline.panel.years=c("2016", "2017", "2019"),
-        sgp.panel.years=c("2016", "2017", "2019"),
+        sgp.baseline.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
+        sgp.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
         sgp.grade.sequences=list(c("5", "6", "8")),
         sgp.baseline.grade.sequences=list(c("5", "6", "8")),
         sgp.projection.baseline.content.areas=rep("MATHEMATICS", 2),
-        sgp.projection.baseline.panel.years=c("2018", "2019"),
+        sgp.projection.baseline.panel.years=c("2017_2018", "2018_2019"),
         sgp.projection.baseline.grade.sequences=list(c("5", "6")),
         sgp.projection.sequence="MATHEMATICS_GRADE_6")
-    MATHEMATICS.2019 = list(
+    MATHEMATICS.2018_2019 = list(
         sgp.content.areas=rep("MATHEMATICS", 3),
         sgp.baseline.content.areas=rep("MATHEMATICS", 3),
-        sgp.baseline.panel.years=c("2016", "2017", "2019"),
-        sgp.panel.years=c("2016", "2017", "2019"),
+        sgp.baseline.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
+        sgp.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
         sgp.grade.sequences=list(c("6", "7", "9")),
         sgp.baseline.grade.sequences=list(c("6", "7", "9")),
         sgp.projection.baseline.content.areas=rep("MATHEMATICS", 2),
-        sgp.projection.baseline.panel.years=c("2018", "2019"),
+        sgp.projection.baseline.panel.years=c("2017_2018", "2018_2019"),
         sgp.projection.baseline.grade.sequences=list(c("6", "7")),
         sgp.projection.sequence="MATHEMATICS_GRADE_7")
-    MATHEMATICS.2019 = list(
+    MATHEMATICS.2018_2019 = list(
         sgp.content.areas=rep("MATHEMATICS", 3),
         sgp.baseline.content.areas=rep("MATHEMATICS", 3),
-        sgp.baseline.panel.years=c("2016", "2017", "2019"),
-        sgp.panel.years=c("2016", "2017", "2019"),
+        sgp.baseline.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
+        sgp.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
         sgp.grade.sequences=list(c("7", "8", "10")),
         sgp.baseline.grade.sequences=list(c("7", "8", "10")),
         sgp.projection.baseline.content.areas=rep("MATHEMATICS", 2),
-        sgp.projection.baseline.panel.years=c("2018", "2019"),
+        sgp.projection.baseline.panel.years=c("2017_2018", "2018_2019"),
         sgp.projection.baseline.grade.sequences=list(c("7", "8")),
         sgp.projection.sequence="MATHEMATICS_GRADE_8")
-    MATHEMATICS.2019 = list(
+    MATHEMATICS.2018_2019 = list(
         sgp.content.areas=rep("MATHEMATICS", 3),
         sgp.baseline.content.areas=rep("MATHEMATICS", 3),
-        sgp.baseline.panel.years=c("2016", "2017", "2019"),
-        sgp.panel.years=c("2016", "2017", "2019"),
+        sgp.baseline.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
+        sgp.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
         sgp.grade.sequences=list(c("8", "9", "11")),
         sgp.baseline.grade.sequences=list(c("8", "9", "11")),
         sgp.projection.baseline.content.areas=rep("MATHEMATICS", 2),
-        sgp.projection.baseline.panel.years=c("2018", "2019"),
+        sgp.projection.baseline.panel.years=c("2017_2018", "2018_2019"),
         sgp.projection.baseline.grade.sequences=list(c("8", "9")),
         sgp.projection.sequence="MATHEMATICS_GRADE_9")
 )
