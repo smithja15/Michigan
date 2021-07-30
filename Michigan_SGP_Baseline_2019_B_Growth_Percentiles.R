ec11073711aef0356159bc9abece7ff4f5a4aa27
Damian Betebenner <dbetebenner@nciea.org>
2021-07-30 09:36:12 -0400

    Getting 2019 Part B underway

--- Michigan_SGP_Baseline_2019_B_Growth_Percentiles.R
+++ Michigan_SGP_Baseline_2019_B_Growth_Percentiles.R
@@ -1,8 +1,8 @@
-################################################################################
-###                                                                          ###
-###   Indiana Learning Loss Analyses -- 2019 Baseline Growth Percentiles     ###
-###                                                                          ###
-################################################################################
+#################################################################################
+###                                                                           ###
+###   Michigan Learning Loss Analyses -- 2019 Baseline Growth Percentiles     ###
+###                                                                           ###
+#################################################################################
 
 ###   Load packages
 require(SGP)
@@ -10,45 +10,47 @@ require(data.table)
 #require(SGPmatrices)
 #debug(addBaselineMatrices)
 
-###   Load data and remove years that will not be used.
-load("Data/Indiana_SGP_LONG_Data.Rdata")
+###   Load data
+load("Data/Michigan_SGP_LONG_Data.Rdata")
 
 ### Test for BASELINE related variable in LONG data and NULL out if they exist
-if (length(tmp.names <- grep("BASELINE|SS", names(Indiana_SGP_LONG_Data))) > 0) {
-		Indiana_SGP_LONG_Data[,eval(tmp.names):=NULL]
+if (length(tmp.names <- grep("BASELINE|SS", names(Michigan_SGP_LONG_Data))) > 0) {
+		Michigan_SGP_LONG_Data[,eval(tmp.names):=NULL]
 }
 
 ###   Add single-cohort baseline matrices to SGPstateData
-SGPstateData <- SGPmatrices::addBaselineMatrices("IN", "2021")
+SGPstateData <- SGPmatrices::addBaselineMatrices("MI", "2021")
 
 ### NULL out assessment transition in 2019 (since already dealth with)
-SGPstateData[["IN"]][["Assessment_Program_Information"]][["Assessment_Transition"]] <- NULL
-SGPstateData[["IN"]][["Assessment_Program_Information"]][["Scale_Change"]] <- NULL
+SGPstateData[["MI"]][["Assessment_Program_Information"]][["Assessment_Transition"]] <- NULL
+SGPstateData[["MI"]][["Assessment_Program_Information"]][["Scale_Change"]] <- NULL
 
 ###   Read in BASELINE percentiles configuration scripts and combine
 source("SGP_CONFIG/2019/BASELINE/Percentiles/ELA.R")
 source("SGP_CONFIG/2019/BASELINE/Percentiles/MATHEMATICS.R")
+source("SGP_CONFIG/2019/BASELINE/Percentiles/SOCIAL_STUDIES.R")
 
-IN_2019_Baseline_Config <- c(
+MI_2019_Baseline_Config <- c(
 	ELA_2019.config,
 	MATHEMATICS_2019.config
+	SOCIAL_STUDIES_2019.config
 )
 
 #####
-###   Run BASELINE SGP analysis - create new Indiana_SGP object with historical data
+###   Run BASELINE SGP analysis - create new Michigan_SGP object with historical data
 #####
 
 ###   Temporarily set names of prior scores from sequential/cohort analyses
-data.table::setnames(Indiana_SGP_LONG_Data,
+data.table::setnames(Michigan_SGP_LONG_Data,
 	c("SCALE_SCORE_PRIOR", "SCALE_SCORE_PRIOR_STANDARDIZED"),
 	c("SS_PRIOR_COHORT", "SS_PRIOR_STD_COHORT"))
 
-SGPstateData[["IN"]][["Assessment_Program_Information"]][["CSEM"]] <- NULL
+SGPstateData[["MI"]][["Assessment_Program_Information"]][["CSEM"]] <- NULL
 
-Indiana_SGP <- abcSGP(
-        sgp_object = Indiana_SGP_LONG_Data,
+Michigan_SGP <- abcSGP(
+        sgp_object = Michigan_SGP_LONG_Data,
         steps = c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
-        sgp.config = IN_2019_Baseline_Config,
+        sgp.config = MI_2019_Baseline_Config,
         sgp.percentiles = FALSE,
         sgp.projections = FALSE,
         sgp.projections.lagged = FALSE,
@@ -62,9 +64,9 @@ Indiana_SGP <- abcSGP(
 )
 
 ###   Re-set and rename prior scores (one set for sequential/cohort, another for skip-year/baseline)
-data.table::setnames(Indiana_SGP@Data,
+data.table::setnames(Michigan_SGP@Data,
   c("SCALE_SCORE_PRIOR", "SCALE_SCORE_PRIOR_STANDARDIZED", "SS_PRIOR_COHORT", "SS_PRIOR_STD_COHORT"),
   c("SCALE_SCORE_PRIOR_BASELINE", "SCALE_SCORE_PRIOR_STANDARDIZED_BASELINE", "SCALE_SCORE_PRIOR", "SCALE_SCORE_PRIOR_STANDARDIZED"))
 
 ###   Save results
-save(Indiana_SGP, file="Data/Indiana_SGP.Rdata")
+save(Michigan_SGP, file="Data/Michigan_SGP.Rdata")
--- SGP_CONFIG/2019/BASELINE/Percentiles/MATHEMATICS.R
+++ SGP_CONFIG/2019/BASELINE/Percentiles/MATHEMATICS.R
@@ -1,13 +1,17 @@
-################################################################################
-###                                                                          ###
-###           Skip-year SGP Configurations for 2019 Math subjects            ###
-###                                                                          ###
-################################################################################
+#############################################################################################
+###                                                                                       ###
+###            Skip-year SGP Configurations for 2018_2019 MATHEMATICS subjects            ###
+###                                                                                       ###
+#############################################################################################
 
-MATHEMATICS_2019.config <- list(
-	MATHEMATICS_2019 = list(
-		sgp.content.areas=rep("MATHEMATICS", 3),
-		sgp.panel.years=c("2016", "2017", "2019"),
-		sgp.grade.sequences=list(c("3", "5"), c("3", "4", "6"), c("4", "5", "7"), c("5", "6", "8")),
-		sgp.norm.group.preference=1L)
+MATHEMATICS.2018_2019.config <- list(
+################## When/If 2015_2016 MATHEMATICS data available, use these configurations
+#	MATHEMATICS.2018_2019 = list(
+#		sgp.content.areas=rep("MATHEMATICS", 3),
+#		sgp.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
+#		sgp.grade.sequences=list(c("3", "5"), c("3", "4", "6"), c("4", "5", "7"), c("5", "6", "8"), c("6", "7", "9"), c("7", "8", "10"), c("8", "9", "11")))
+	MATHEMATICS.2018_2019 = list(
+		sgp.content.areas=rep("MATHEMATICS", 2),
+		sgp.panel.years=c("2016_2017", "2018_2019"),
+		sgp.grade.sequences=list(c("3", "5"), c("4", "6"), c("5", "7"), c("6", "8"), c("7", "9"), c("8", "10"), c("9", "11")))
 )
