5bb482eb0760f28ce905d582cd0819afe10ec8dd
Damian Betebenner <dbetebenner@nciea.org>
2021-08-28 21:25:34 -0400

    Adding in the 8 to 11 configurations

--- SGP_CONFIG/2018_2019/BASELINE/Percentiles/MATHEMATICS.R
+++ SGP_CONFIG/2018_2019/BASELINE/Percentiles/MATHEMATICS.R
@@ -13,5 +13,11 @@ MATHEMATICS.2018_2019.config <- list(
 	MATHEMATICS.2018_2019 = list(
 		sgp.content.areas=rep("MATHEMATICS", 2),
 		sgp.panel.years=c("2016_2017", "2018_2019"),
-		sgp.grade.sequences=list(c("3", "5"), c("4", "6"), c("5", "7"), c("6", "8"), c("7", "9"), c("8", "10"), c("9", "11")))
+		sgp.grade.sequences=list(c("3", "5"), c("4", "6"), c("5", "7"), c("6", "8"), c("7", "9"), c("8", "10"), c("9", "11")),
+		sgp.norm.group.preference=1),
+	MATHEMATICS.2018_2019 = list(
+		sgp.content.areas=rep("MATHEMATICS", 2),
+		sgp.panel.years=c("2015_2016", "2018_2019"),
+		sgp.grade.sequences=list(c("8", "11")),
+		sgp.norm.group.preference=2)
 )
--- SGP_CONFIG/2018_2019/BASELINE/Percentiles/READING.R
+++ SGP_CONFIG/2018_2019/BASELINE/Percentiles/READING.R
@@ -13,5 +13,11 @@ READING.2018_2019.config <- list(
 	READING.2018_2019 = list(
 		sgp.content.areas=rep("READING", 2),
 		sgp.panel.years=c("2016_2017", "2018_2019"),
-		sgp.grade.sequences=list(c("3", "5"), c("4", "6"), c("5", "7"), c("6", "8"), c("7", "9"), c("8", "10"), c("9", "11")))
+		sgp.grade.sequences=list(c("3", "5"), c("4", "6"), c("5", "7"), c("6", "8"), c("7", "9"), c("8", "10"), c("9", "11")),
+		sgp.norm.grooup.preference=1),
+	READING.2018_2019 = list(
+		sgp.content.areas=rep("READING", 2),
+		sgp.panel.years=c("2015_2016", "2018_2019"),
+		sgp.grade.sequences=list(c("8", "11")),
+		sgp.norm.grooup.preference=2)
 )
