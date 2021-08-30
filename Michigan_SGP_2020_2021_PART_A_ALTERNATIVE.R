201afae9d24f38c3c76760695a1383a9f5a12465
Damian Betebenner <dbetebenner@nciea.org>
2021-08-29 21:13:37 -0400

    Fixing up the configuration and the Alternative script

--- Michigan_SGP_2020_2021_PART_A_ALTERNATIVE.R
+++ Michigan_SGP_2020_2021_PART_A_ALTERNATIVE.R
@@ -1,22 +1,23 @@
 ######################################################################################
 ###                                                                                ###
 ###                Michigan COVID Skip-year SGP analyses for 2020-2021             ###
+###                USING LONG DATA SUPPLIED BY MIDOE/DRC
 ###                                                                                ###
 ######################################################################################
 
-###   Load packages
+### Load packages
 require(data.table)
 require(SGP)
 require(SGPmatrices)
 
-###   Load data
+### Load data
 Michigan_Data_LONG <- load("Data/MStep_2021_w_Equated_Scores.Rdata")
 
-###   Add Baseline matrices to SGPstateData
+### Add Baseline matrices to SGPstateData
 SGPstateData <- addBaselineMatrices("MI", "2020_2021")
 SGPstateData[["MI"]][["Assessment_Program_Information"]][["Assessment_Transition"]] <- NULL
 
-###   Read in SGP Configuration Scripts and Combine
+### Read in SGP Configuration Scripts and Combine
 source("SGP_CONFIG/2020_2021/PART_A/READING.R")
 source("SGP_CONFIG/2020_2021/PART_A/MATHEMATICS.R")
 source("SGP_CONFIG/2020_2021/PART_A/SOCIAL_STUDIES.R")
@@ -26,12 +27,26 @@ MI_2020_2021.config <- c(READING_2020_2021.config, MATHEMATICS_2020_2021.config,
 ### Parameters
 parallel.config <- list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=8, BASELINE_PERCENTILES=8, PROJECTIONS=8, LAGGED_PROJECTIONS=8, SGP_SCALE_SCORE_TARGETS=8))
 
-#####
-###   Run abcSGP analysis for cohort referenced SGP
-#####
+######
+###### STEP 1: CALCULATE COHORT REFERENCED SGPS
+######
 
+### TEMPORARILY MODIFY KNOTS & BOUNDARIES TO ACCOMODATE NON-EQUATED SCORES
+SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["MATHEMATICS.2018_2019"]] <- SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["MATHEMATICS"]]
+SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["READING.2018_2019"]] <- SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["READING"]]
+SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["SOCIAL_STUDIES.2018_2019"]] <- SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["SOCIAL_STUDIES"]]
+SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["MATHEMATICS"]][["boundaries_8"]] <- c( 1712.5, 1862.5)
+SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["MATHEMATICS"]][["knots_8"]] <- c(1767, 1782, 1796, 1813)
+SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["MATHEMATICS"]][["loss.hoss_8"]] <- c(1725, 1850)
+SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["READING"]][["boundaries_8"]] <- c(1707.4, 1870.6)
+SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["READING"]][["knots_8"]] <- c(1774, 1791, 1806, 1822)
+SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["READING"]][["loss.hoss_8"]] <- c(1721, 1857)
+
+
+###   Run abcSGP analysis for cohort referenced SGP
 Michigan_SGP <- abcSGP(
-        sgp_object = Michigan_Data_LONG,
+        sgp_object = MStep_2021_w_Equated_Scores,
+        state = "MI",
         steps = c("prepareSGP", "analyzeSGP", "combineSGP"),
         sgp.config = MI_2020_2021.config,
         sgp.percentiles = TRUE,
@@ -45,13 +60,19 @@ Michigan_SGP <- abcSGP(
 )
 
 
-#####
-###   Run abcSGP analysis for cohort referenced SGP
-#####
+######
+###### STEP 2: CALCULATE BASELINE REFERENCED SGPS
+######
 
-#Temporarily swap SCALE_SCORE_EQUATED with SCALE_SCORE to run baseline SGPs
+### Setup knots and boundaries
+SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["MATHEMATICS"]] <- SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["MATHEMATICS.2018_2019"]]
+SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["READING"]] <- SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["READING.2018_2019"]]
+SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["SOCIAL_STUDIES"]] <- SGPstateData[["MI"]][["Achievement"]][["Knots_Boundaries"]][["SOCIAL_STUDIES.2018_2019"]]
+
+### Temporarily swap SCALE_SCORE_EQUATED with SCALE_SCORE to run baseline SGPs
 setnames(Michigan_SGP@Data, c("SCALE_SCORE", "SCALE_SCORE_EQUATED"), c("SCALE_SCORE_EQUATED", "SCALE_SCORE"))
 
+###   Run abcSGP analysis for baseline referenced SGP
 Michigan_SGP <- abcSGP(
         sgp_object = Michigan_SGP,
         steps = c("prepareSGP", "analyzeSGP", "combineSGP"),
@@ -67,12 +88,10 @@ Michigan_SGP <- abcSGP(
 )
 
 
-#Swap back SCALE_SCORE_EQUATED with SCALE_SCORE to run baseline SGPs
+### Swap back SCALE_SCORE_EQUATED with SCALE_SCORE to run baseline SGPs
 setnames(Michigan_SGP@Data, c("SCALE_SCORE_EQUATED", "SCALE_SCORE"), c("SCALE_SCORE", "SCALE_SCORE_EQUATED"))
 
-
 ### Copy SCALE_SCORE_PRIOR and SCALE_SCORE_PRIOR_STANDARDIZED to BASELINE counter parts
-
 Michigan_SGP@Data[YEAR=="2020_2021", SCALE_SCORE_PRIOR_BASELINE:=SCALE_SCORE_PRIOR]
 Michigan_SGP@Data[YEAR=="2020_2021", SCALE_SCORE_PRIOR_STANDARDIZED_BASELINE:=SCALE_SCORE_PRIOR_STANDARDIZED]
 
--- SGP_CONFIG/2020_2021/PART_A/READING.R
+++ SGP_CONFIG/2020_2021/PART_A/READING.R
@@ -6,8 +6,8 @@ READING_2020_2021.config <- list(
      READING.2020_2021 = list(
                  sgp.content.areas = c('READING', 'READING', 'READING'),
                  sgp.panel.years = c('2017_2018', '2018_2019', '2020_2021'),
-                 sgp.grade.sequences = list(c('3', '5'), c('3', '4', '6'), c('4', '5', '7'), c('5', '6', '8'), c('6', '7', '9'), c('7', '8', '10')))
-         		#This is used operationally for grade 11
+                 sgp.grade.sequences = list(c('3', '5'), c('3', '4', '6'), c('4', '5', '7'), c('5', '6', '8'), c('6', '7', '9'), c('7', '8', '10'))),
+    #This is used operationally for grade 11
     READING_SAT.2020_2021=list(
          			sgp.content.areas=c('READING', 'READING', 'READING'),
          			sgp.panel.years=c('2016_2017', '2017_2018', '2020_2021'),
