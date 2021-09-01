161648b803c5b9d49ce0741846bbb4bdf2e0cd47
smithja15 <53953307+smithja15@users.noreply.github.com>
2021-09-01 17:06:29 -0400

    Update SOCIAL_STUDIES.R

--- SGP_CONFIG/2020_2021/PART_A/SOCIAL_STUDIES.R
+++ SGP_CONFIG/2020_2021/PART_A/SOCIAL_STUDIES.R
@@ -6,5 +6,9 @@ SOCIAL_STUDIES_2020_2021.config <- list(
      SOCIAL_STUDIES.2020_2021 = list(
                  sgp.content.areas = c('SOCIAL_STUDIES', 'SOCIAL_STUDIES', 'SOCIAL_STUDIES'),
                  sgp.panel.years = c('2014_2015', '2017_2018', '2020_2021'),
-                 sgp.grade.sequences = list(c('5', '8', '11')))
+                 sgp.grade.sequences = list(c('5', '8', '11'))), 
+     SOCIAL_STUDIES.2020_2021 = list(
+                 sgp.content.areas = c('SOCIAL_STUDIES', 'SOCIAL_STUDIES'),
+                 sgp.panel.years = c('2017_2018', '2020_2021'),
+                 sgp.grade.sequences = list(c('5', '8')))
 )
