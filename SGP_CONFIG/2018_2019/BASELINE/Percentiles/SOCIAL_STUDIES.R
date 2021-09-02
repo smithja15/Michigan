138f3bcdfb996bb185242c5f07afba9fe28d9f2a
Damian Betebenner <dbetebenner@nciea.org>
2021-09-01 21:32:30 -0400

    Fixing up SOCIAL_STUDIES configurations

--- SGP_CONFIG/2018_2019/BASELINE/Percentiles/SOCIAL_STUDIES.R
+++ SGP_CONFIG/2018_2019/BASELINE/Percentiles/SOCIAL_STUDIES.R
@@ -6,9 +6,9 @@
 
 SOCIAL_STUDIES.2018_2019.config <- list(
 	SOCIAL_STUDIES.2018_2019=list(
-		sgp.content.areas=c('SOCIAL_STUDIES', 'SOCIAL_STUDIES', 'SOCIAL_STUDIES'),
-		sgp.panel.years=c('2013_2014', '2015_2016', '2018_2019'),
-		sgp.grade.sequences=list(c('6', '8', '11'))),
+		sgp.content.areas=c('SOCIAL_STUDIES', 'SOCIAL_STUDIES'),
+		sgp.panel.years=c('2015_2016', '2018_2019'),
+		sgp.grade.sequences=list(c('8', '11'))),
 	SOCIAL_STUDIES.2018_2019=list(
 		sgp.content.areas=c('SOCIAL_STUDIES', 'SOCIAL_STUDIES', 'SOCIAL_STUDIES'),
 		sgp.panel.years=c('2015_2016', '2018_2019'),
