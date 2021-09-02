7aad12ca12a515fc384d0c5928b260a8aa44d49b
Damian Betebenner <dbetebenner@nciea.org>
2021-09-01 20:21:14 -0400

    Adding in 5 to 8 configuration for baseline SGP coefficient matrices

--- SGP_CONFIG/2018_2019/BASELINE/Matrices/SOCIAL_STUDIES.R
+++ SGP_CONFIG/2018_2019/BASELINE/Matrices/SOCIAL_STUDIES.R
@@ -9,5 +9,10 @@ SOCIAL_STUDIES_BASELINE.config <- list(
 		sgp.baseline.content.areas=rep("SOCIAL_STUDIES", 2),
 		sgp.baseline.panel.years=c("2015_2016", "2018_2019"),
 		sgp.baseline.grade.sequences=c("8", "11"),
+		sgp.baseline.grade.sequences.lags=3),
+	list(
+		sgp.baseline.content.areas=rep("SOCIAL_STUDIES", 2),
+		sgp.baseline.panel.years=c("2015_2016", "2018_2019"),
+		sgp.baseline.grade.sequences=c("5", "8"),
 		sgp.baseline.grade.sequences.lags=3)
 )
