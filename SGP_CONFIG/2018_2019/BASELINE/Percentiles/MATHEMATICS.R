cff4f1b6accfeb447c1b262119dc3f0f880e7317
Damian Betebenner <dbetebenner@nciea.org>
2022-08-12 07:28:26 -0400

    Adding in skip-2-year configurations for sgp calculations

--- SGP_CONFIG/2018_2019/BASELINE/Percentiles/MATHEMATICS.R
+++ SGP_CONFIG/2018_2019/BASELINE/Percentiles/MATHEMATICS.R
@@ -4,20 +4,22 @@
 ###                                                                                       ###
 #############################################################################################
 
-MATHEMATICS.2018_2019.config <- list(
-################## When/If 2015_2016 MATHEMATICS data available, use these configurations
-#	MATHEMATICS.2018_2019 = list(
-#		sgp.content.areas=rep("MATHEMATICS", 3),
-#		sgp.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
-#		sgp.grade.sequences=list(c("3", "5"), c("3", "4", "6"), c("4", "5", "7"), c("5", "6", "8"), c("6", "7", "9"), c("7", "8", "10"), c("8", "9", "11")))
+MATHEMATICS.2018_2019_SKIP_YEAR.config <- list(
 	MATHEMATICS.2018_2019 = list(
-		sgp.content.areas=rep("MATHEMATICS", 2),
-		sgp.panel.years=c("2016_2017", "2018_2019"),
-		sgp.grade.sequences=list(c("3", "5"), c("4", "6"), c("5", "7"), c("6", "8"), c("7", "9"), c("8", "10"), c("9", "11")),
+		sgp.content.areas=rep("MATHEMATICS", 3),
+		sgp.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
+		sgp.grade.sequences=list(c("3", "5"), c("3", "4", "6"), c("4", "5", "7"), c("5", "6", "8"), c("6", "7", "9"), c("7", "8", "10"), c("8", "9", "11")),
 		sgp.norm.group.preference=1),
 	MATHEMATICS.2018_2019 = list(
 		sgp.content.areas=rep("MATHEMATICS", 2),
 		sgp.panel.years=c("2015_2016", "2018_2019"),
 		sgp.grade.sequences=list(c("8", "11")),
 		sgp.norm.group.preference=2)
+)
+
+MATHEMATICS.2018_2019_SKIP_2_YEAR.config <- list(
+	MATHEMATICS.2018_2019 = list(
+		sgp.content.areas=rep("MATHEMATICS", 3),
+		sgp.panel.years=c("2014_2015", "2015_2016", "2018_2019"),
+		sgp.grade.sequences=list(c("3", "6"), c("3", "4", "7"), c("4", "5", "8"), c("5", "6", "9"), c("6", "7", "10"), c("7", "8", "11")))
 )
--- SGP_CONFIG/2018_2019/BASELINE/Percentiles/READING.R
+++ SGP_CONFIG/2018_2019/BASELINE/Percentiles/READING.R
@@ -5,19 +5,21 @@
 #########################################################################################
 
 READING.2018_2019.config <- list(
-################## When/If 2015_2016 READING data available, use these configurations
-#	READING.2018_2019 = list(
-#		sgp.content.areas=rep("READING", 3),
-#		sgp.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
-#		sgp.grade.sequences=list(c("3", "5"), c("3", "4", "6"), c("4", "5", "7"), c("5", "6", "8"), c("6", "7", "9"), c("7", "8", "10"), c("8", "9", "11")))
 	READING.2018_2019 = list(
-		sgp.content.areas=rep("READING", 2),
-		sgp.panel.years=c("2016_2017", "2018_2019"),
-		sgp.grade.sequences=list(c("3", "5"), c("4", "6"), c("5", "7"), c("6", "8"), c("7", "9"), c("8", "10"), c("9", "11")),
+		sgp.content.areas=rep("READING", 3),
+		sgp.panel.years=c("2015_2016", "2016_2017", "2018_2019"),
+		sgp.grade.sequences=list(c("3", "5"), c("3", "4", "6"), c("4", "5", "7"), c("5", "6", "8"), c("6", "7", "9"), c("7", "8", "10"), c("8", "9", "11")),
 		sgp.norm.grooup.preference=1),
 	READING.2018_2019 = list(
 		sgp.content.areas=rep("READING", 2),
 		sgp.panel.years=c("2015_2016", "2018_2019"),
 		sgp.grade.sequences=list(c("8", "11")),
 		sgp.norm.grooup.preference=2)
+)
+
+READING.2018_2019_SKIP_2_YEAR.config <- list(
+	READING.2018_2019 = list(
+		sgp.content.areas=rep("READING", 3),
+		sgp.panel.years=c("2014_2015", "2015_2016", "2018_2019"),
+		sgp.grade.sequences=list(c("3", "6"), c("3", "4", "7"), c("4", "5", "8"), c("5", "6", "9"), c("6", "7", "10"), c("7", "8", "11")))
 )
