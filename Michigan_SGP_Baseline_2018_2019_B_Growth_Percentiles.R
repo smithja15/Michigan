9ef2e4afca8800fdc70a092d2d3e2b3b2e36677e
Damian Betebenner <dbetebenner@nciea.org>
2022-08-13 16:39:47 -0400

    Update to Part B of 2018_2019 percentile calculation

--- Michigan_SGP_Baseline_2018_2019_B_Growth_Percentiles.R
+++ Michigan_SGP_Baseline_2018_2019_B_Growth_Percentiles.R
@@ -9,7 +9,7 @@ require(SGP)
 require(data.table)
 
 ###   Load data
-load("Data/Michigan_SGP.Rdata")
+load("Data/Base_Files/Michigan_SGP.Rdata")
 
 ### Test for BASELINE related variable in LONG data and NULL out if they exist
 if (length(tmp.names <- grep("BASELINE|SS", names(Michigan_SGP@Data))) > 0) {
@@ -29,19 +29,19 @@ source("SGP_CONFIG/2018_2019/BASELINE/Percentiles/MATHEMATICS.R")
 source("SGP_CONFIG/2018_2019/BASELINE/Percentiles/SOCIAL_STUDIES.R")
 
 MI_2018_2019_Baseline_Config_CONSECUTIVE_YEAR <- c(
-	READING_CONSECUTIVE_YEAR.2018_2019.config,
-	MATHEMATICS_CONSECUTIVE_YEAR.2018_2019.config,
+	READING.2018_2019.config,
+	MATHEMATICS.2018_2019.config
 )
 
 MI_2018_2019_Baseline_Config_SKIP_YEAR <- c(
-	READING_SKIP_YEAR.2018_2019.config,
-	MATHEMATICS_SKIP_YEAR.2018_2019.config,
+	READING.2018_2019_SKIP_YEAR.config,
+	MATHEMATICS.2018_2019_SKIP_YEAR.config
 )
 
 MI_2018_2019_Baseline_Config_SKIP_2_YEAR <- c(
-	READING_SKIP_2_YEAR.2018_2019.config,
-	MATHEMATICS_SKIP_2_YEAR.2018_2019.config,
-	SOCIAL_STUDIES_SKIP_2_YEAR.2018_2019.config,
+	READING.2018_2019_SKIP_2_YEAR.config,
+	MATHEMATICS.2018_2019_SKIP_2_YEAR.config,
+	SOCIAL_STUDIES.2018_2019_SKIP_2_YEAR.config
 )
 
 SGPstateData[["MI"]][["Assessment_Program_Information"]][["CSEM"]] <- NULL
@@ -52,22 +52,26 @@ SGPstateData[["MI"]][["Assessment_Program_Information"]][["CSEM"]] <- NULL
 #####
 
 Michigan_SGP <- abcSGP(
-        sgp_object = Michigan_SGP_LONG_Data,
-        steps = c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
+        sgp_object = Michigan_SGP,
+        steps = c("prepareSGP", "analyzeSGP", "combineSGP"),
         sgp.config = MI_2018_2019_Baseline_Config_SKIP_2_YEAR,
         sgp.percentiles = FALSE,
         sgp.projections = FALSE,
         sgp.projections.lagged = FALSE,
-        sgp.percentiles.baseline = TRUE,  #  Skip year SGPs for 2021 comparisons
+        sgp.percentiles.baseline = TRUE,  #  Skip 2 year SGPs for 2022 comparisons
         sgp.projections.baseline = FALSE, #  Calculated in next step
         sgp.projections.lagged.baseline = FALSE,
         save.intermediate.results = FALSE,
         parallel.config = list(
 					BACKEND = "PARALLEL",
-					WORKERS=list(BASELINE_PERCENTILES=8))
+					WORKERS=list(BASELINE_PERCENTILES=12))
 )
 
+baseline.names <- grep("BASELINE", names(Michigan_SGP@Data), value = TRUE)
+setnames(Michigan_SGP@Data, baseline.names, paste0(baseline.names, "_SKIP_2_YEAR"))
 
+sgps.2018_2019 <- grep(".2018_2019.BASELINE", names(Michigan_SGP@SGP[["SGPercentiles"]]))
+names(Michigan_SGP@SGP[["SGPercentiles"]])[sgps.2018_2019] <- gsub(".2018_2019.BASELINE", ".2018_2019.SKIP_2_YEAR_BLINE", names(Michigan_SGP@SGP[["SGPercentiles"]])[sgps.2018_2019])
 
 #####
 ###   Run BASELINE SGP analysis for SKIP_YEAR (one-year skip) 
@@ -75,8 +79,8 @@ Michigan_SGP <- abcSGP(
 #####
 
 Michigan_SGP <- abcSGP(
-        sgp_object = Michigan_SGP_LONG_Data,
-        steps = c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
+        sgp_object = Michigan_SGP,
+        steps = c("prepareSGP", "analyzeSGP", "combineSGP"),
         sgp.config = MI_2018_2019_Baseline_Config_SKIP_YEAR,
         sgp.percentiles = FALSE,
         sgp.projections = FALSE,
@@ -90,19 +94,25 @@ Michigan_SGP <- abcSGP(
 					WORKERS=list(BASELINE_PERCENTILES=8))
 )
 
+baseline.names <- setdiff(grep("BASELINE", names(Michigan_SGP@Data), value = TRUE), grep("BASELINE_SKIP_2_YEAR", names(Michigan_SGP@Data), value = TRUE))
+setnames(Michigan_SGP@Data, baseline.names, paste0(baseline.names, "_SKIP_YEAR"))
+
+sgps.2018_2019 <- grep(".2018_2019.BASELINE", names(Michigan_SGP@SGP[["SGPercentiles"]]))
+names(Michigan_SGP@SGP[["SGPercentiles"]])[sgps.2018_2019] <- gsub(".2018_2019.BASELINE", ".2018_2019.SKIP_YEAR_BLINE", names(Michigan_SGP@SGP[["SGPercentiles"]])[sgps.2018_2019])
+
 #####
 ###   Run BASELINE SGP analysis for consecutive year
 ###   create new Michigan_SGP object with historical data
 #####
 
 Michigan_SGP <- abcSGP(
-        sgp_object = Michigan_SGP_LONG_Data,
+        sgp_object = Michigan_SGP,
         steps = c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
         sgp.config = MI_2018_2019_Baseline_Config,
         sgp.percentiles = FALSE,
         sgp.projections = FALSE,
         sgp.projections.lagged = FALSE,
-        sgp.percentiles.baseline = TRUE,  #  Skip year SGPs for 2021 comparisons
+        sgp.percentiles.baseline = TRUE,  #  Consecutive year SGPs for 2021 comparisons
         sgp.projections.baseline = FALSE, #  Calculated in next step
         sgp.projections.lagged.baseline = FALSE,
         save.intermediate.results = FALSE,
@@ -114,4 +124,4 @@ Michigan_SGP <- abcSGP(
 
 
 ###   Save results
-save(Michigan_SGP, file="Data/Michigan_SGP.Rdata")
+#save(Michigan_SGP, file="Data/Michigan_SGP.Rdata")
