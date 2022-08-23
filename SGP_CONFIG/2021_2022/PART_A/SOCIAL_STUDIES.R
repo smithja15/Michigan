1284686f5224961aad0803351bcbea980ad34c83
Damian Betebenner <dbetebenner@nciea.org>
2022-08-23 14:45:25 -0400

    Fixing configuration syntax

--- SGP_CONFIG/2021_2022/PART_A/SOCIAL_STUDIES.R
+++ SGP_CONFIG/2021_2022/PART_A/SOCIAL_STUDIES.R
@@ -15,7 +15,7 @@ SOCIAL_STUDIES.2021_2022.config <- list(
 	SOCIAL_STUDIES.2021_2022=list(
 		sgp.content.areas=c('SOCIAL_STUDIES', 'SOCIAL_STUDIES'),
 		sgp.panel.years=c('2018_2019', '2021_2022'),
-		sgp.grade.sequences=list(c('8', '11'))
+		sgp.grade.sequences=list(c('8', '11')))
 )
 
 SOCIAL_STUDIES.2021_2022_SKIP_2_YEAR.config <- list(
