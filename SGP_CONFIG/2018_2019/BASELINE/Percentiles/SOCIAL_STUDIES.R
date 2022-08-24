baa42203819b77f563397ae03255b4689dc3f200
smithja15 <53953307+smithja15@users.noreply.github.com>
2022-08-24 18:18:44 -0400

    Update SOCIAL_STUDIES.R

--- SGP_CONFIG/2018_2019/BASELINE/Percentiles/SOCIAL_STUDIES.R
+++ SGP_CONFIG/2018_2019/BASELINE/Percentiles/SOCIAL_STUDIES.R
@@ -16,6 +16,12 @@ SOCIAL_STUDIES.2018_2019.config <- list(
 	SOCIAL_STUDIES.2018_2019=list(
 		sgp.content.areas=c('SOCIAL_STUDIES', 'SOCIAL_STUDIES'),
 		sgp.panel.years=c('2015_2016', '2018_2019'),
+		sgp.grade.sequences=list(c('5', '8')),
+		sgp.projection.baseline.grade.sequences="NO_PROJECTIONS"),
+	
+	SOCIAL_STUDIES.2018_2019=list(
+		sgp.content.areas=c('SOCIAL_STUDIES', 'SOCIAL_STUDIES'),
+		sgp.panel.years=c('2015_2016', '2018_2019'),
 		sgp.grade.sequences=list(c('8', '11')),
 		sgp.projection.baseline.grade.sequences="NO_PROJECTIONS")
 )
@@ -31,5 +37,10 @@ SOCIAL_STUDIES.2018_2019_SKIP_2_YEAR.config <- list(
 	SOCIAL_STUDIES.2018_2019=list(
 		sgp.content.areas=c('SOCIAL_STUDIES', 'SOCIAL_STUDIES'),
 		sgp.panel.years=c('2015_2016', '2018_2019'),
+		sgp.grade.sequences=list(c('8', '5')),
+	
+	SOCIAL_STUDIES.2018_2019=list(
+		sgp.content.areas=c('SOCIAL_STUDIES', 'SOCIAL_STUDIES'),
+		sgp.panel.years=c('2015_2016', '2018_2019'),
 		sgp.grade.sequences=list(c('8', '11')))
-)
+))
