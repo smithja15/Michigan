9e65123148a1c5bf148881e79d7a5e04be395f03
Damian Betebenner <dbetebenner@nciea.org>
2022-08-23 22:11:01 -0400

    Updating PART_B and PART_C of 2021-2022 analyses

--- SGP_CONFIG/2021_2022/PART_C/READING.R
+++ SGP_CONFIG/2021_2022/PART_C/READING.R
@@ -1,71 +1,54 @@
 #####
-###   Configurations for calculating LAGGED PROJECTIONS in 2020_2021
+###   Configurations for calculating LAGGED PROJECTIONS in 2021_2022
 #####
 
-
-READING_2020_2021.config <- list(
-     READING.2020_2021 = list(
+READING_2021_2022.config <- list(
+     READING.2021_2022 = list(
                  sgp.content.areas=c("READING", "READING"),
                  sgp.baseline.content.areas=c("READING", "READING"),
-                 sgp.panel.years=c("2018_2019", "2020_2021"),
-                 sgp.baseline.panel.years=c("2018_2019", "2020_2021"),
-                 sgp.grade.sequences=list(c("3", "5")),
-                 sgp.baseline.grade.sequences=list(c("3", "5")),
-                 sgp.projection.sequence="READING_GRADE_5"),
-     READING.2020_2021 = list(
-                 sgp.content.areas=c("READING", "READING", "READING"),
-                 sgp.baseline.content.areas=c("READING", "READING", "READING"),
-                 sgp.baseline.panel.years=c("2017_2018", "2018_2019", "2020_2021"),
-                 sgp.panel.years=c("2017_2018", "2018_2019", "2020_2021"),
-                 sgp.grade.sequences=list(c("3", "4", "6")),
-                 sgp.baseline.grade.sequences=list(c("3", "4", "6")),
+                 sgp.panel.years=c("2018_2019", "2021_2022"),
+                 sgp.baseline.panel.years=c("2018_2019", "2021_2022"),
+                 sgp.grade.sequences=list(c("3", "6")),
+                 sgp.baseline.grade.sequences=list(c("3", "6")),
                  sgp.projection.sequence="READING_GRADE_6"),
-     READING.2020_2021 = list(
+     READING.2021_2022 = list(
                  sgp.content.areas=c("READING", "READING", "READING"),
                  sgp.baseline.content.areas=c("READING", "READING", "READING"),
-                 sgp.baseline.panel.years=c("2017_2018", "2018_2019", "2020_2021"),
-                 sgp.panel.years=c("2017_2018", "2018_2019", "2020_2021"),
-                 sgp.grade.sequences=list(c("4", "5", "7")),
-                 sgp.baseline.grade.sequences=list(c("4", "5", "7")),
+                 sgp.baseline.panel.years=c("2017_2018", "2018_2019", "2021_2022"),
+                 sgp.panel.years=c("2017_2018", "2018_2019", "2021_2022"),
+                 sgp.grade.sequences=list(c("3", "4", "7")),
+                 sgp.baseline.grade.sequences=list(c("3", "4", "7")),
                  sgp.projection.sequence="READING_GRADE_7"),
-     READING.2020_2021 = list(
+     READING.2021_2022 = list(
                  sgp.content.areas=c("READING", "READING", "READING"),
                  sgp.baseline.content.areas=c("READING", "READING", "READING"),
-                 sgp.baseline.panel.years=c("2017_2018", "2018_2019", "2020_2021"),
-                 sgp.panel.years=c("2017_2018", "2018_2019", "2020_2021"),
-                 sgp.grade.sequences=list(c("5", "6", "8")),
-                 sgp.baseline.grade.sequences=list(c("5", "6", "8")),
+                 sgp.baseline.panel.years=c("2017_2018", "2018_2019", "2021_2022"),
+                 sgp.panel.years=c("2017_2018", "2018_2019", "2021_2022"),
+                 sgp.grade.sequences=list(c("4", "5", "8")),
+                 sgp.baseline.grade.sequences=list(c("4", "5", "8")),
                  sgp.projection.sequence="READING_GRADE_8"),
-     READING.2020_2021 = list(
+     READING.2021_2022 = list(
                  sgp.content.areas=c("READING", "READING", "READING"),
                  sgp.baseline.content.areas=c("READING", "READING", "READING"),
-                 sgp.baseline.panel.years=c("2017_2018", "2018_2019", "2020_2021"),
-                 sgp.panel.years=c("2017_2018", "2018_2019", "2020_2021"),
-                 sgp.grade.sequences=list(c("6", "7", "9")),
-                 sgp.baseline.grade.sequences=list(c("6", "7", "9")),
+                 sgp.baseline.panel.years=c("2017_2018", "2018_2019", "2021_2022"),
+                 sgp.panel.years=c("2017_2018", "2018_2019", "2021_2022"),
+                 sgp.grade.sequences=list(c("5", "6", "9")),
+                 sgp.baseline.grade.sequences=list(c("5", "6", "9")),
                  sgp.projection.sequence="READING_GRADE_9"),
-     READING.2020_2021 = list(
+     READING.2021_2022 = list(
                  sgp.content.areas=c("READING", "READING", "READING"),
                  sgp.baseline.content.areas=c("READING", "READING", "READING"),
-                 sgp.baseline.panel.years=c("2017_2018", "2018_2019", "2020_2021"),
-                 sgp.panel.years=c("2017_2018", "2018_2019", "2020_2021"),
-                 sgp.grade.sequences=list(c("7", "8", "10")),
-                 sgp.baseline.grade.sequences=list(c("7", "8", "10")),
+                 sgp.baseline.panel.years=c("2017_2018", "2018_2019", "2021_2022"),
+                 sgp.panel.years=c("2017_2018", "2018_2019", "2021_2022"),
+                 sgp.grade.sequences=list(c("6", "7", "10")),
+                 sgp.baseline.grade.sequences=list(c("6", "7", "10")),
                  sgp.projection.sequence="READING_GRADE_10"),
-     READING.2020_2021 = list(
+     READING.2021_2022 = list(
                  sgp.content.areas=c("READING", "READING", "READING"),
                  sgp.baseline.content.areas=c("READING", "READING", "READING"),
-                 sgp.baseline.panel.years=c("2017_2018", "2018_2019", "2020_2021"),
-                 sgp.panel.years=c("2017_2018", "2018_2019", "2020_2021"),
-                 sgp.grade.sequences=list(c("8", "9", "11")),
-                 sgp.baseline.grade.sequences=list(c("8", "9", "11")),
-                 sgp.projection.sequence="READING_GRADE_11"),
-     READING.2020_2021 = list(
-                 sgp.content.areas=c("READING", "READING", "READING"),
-                 sgp.baseline.content.areas=c("READING", "READING", "READING"),
-                 sgp.baseline.panel.years=c("2016_2017", "2017_2018", "2020_2021"),
-                 sgp.panel.years=c("2016_2017", "2017_2018", "2020_2021"),
+                 sgp.baseline.panel.years=c("2017_2018", "2018_2019", "2021_2022"),
+                 sgp.panel.years=c("2017_2018", "2018_2019", "2021_2022"),
                  sgp.grade.sequences=list(c("7", "8", "11")),
                  sgp.baseline.grade.sequences=list(c("7", "8", "11")),
-                 sgp.projection.sequence="READING_GRADE_8_to_11")
+                 sgp.projection.sequence="READING_GRADE_11")
 )
